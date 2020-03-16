0df6a0319197fe428de3768b4e7981d6ee401e36
b.aktas <b.aktas@jrtechnologies.com>
2020-03-16 20:19:58 +0300

    initial

---
+++ api/models/BaseEntity.go
@@ -0,0 +1,8 @@
+package models
+
+import "time"
+
+type BaseEntity struct {
+	CreatedAt time.Time `gorm:"default:current_timestamp()" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:current_timestamp()" json:"updated_at"`
+}
